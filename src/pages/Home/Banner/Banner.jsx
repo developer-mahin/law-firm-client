642e441c4055757fc4cbfba694f5bc244a56b51f
developer-mahin <mdmahin1310@gmail.com>
2022-11-08 00:01:44 +0600

    added banner section

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Banner.css";
 
 const Banner = () => {
   return (
     <div className="relative">
       <img
-        src="https://images.pexels.com/photos/3228766/pexels-photo-3228766.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+        src="https://i.ibb.co/gg2zhTr/banner-1.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
         className="absolute inset-0 object-cover w-full h-full"
         alt=""
       />
-      <div className="relative bg-opacity-75 bg-deep-purple-accent-700">
+      <div className="relative bg-opacity-75 banner-image">
         <svg
           className="absolute inset-x-0 bottom-0 text-white"
           viewBox="0 0 1160 163"
@@ -22,18 +24,17 @@ const Banner = () => {
           <div className="flex flex-col items-center justify-between xl:flex-row">
             <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
               <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                The quick, brown fox <br className="hidden md:block" />
-                jumps over a lazy dog
+                Professional Law Firm
               </h2>
               <p className="max-w-xl mb-4 text-base text-gray-200 md:text-lg">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium doloremque laudan, totam rem aperiam, eaque ipsa
-                quae.
+                Our Law Firm in Bangladesh is a full service law firm and has
+                adequate knowledge, expertise, lawyers, consultants and
+                logistics to undertake any legal, para legal or related service.
               </p>
-              <a
-                href="/"
+              <Link
+                to="/"
                 aria-label=""
-                className="inline-flex items-center font-semibold tracking-wider transition-colors duration-200 text-teal-accent-400 hover:text-teal-accent-700"
+                className="inline-flex items-center font-semibold tracking-wider transition-colors duration-200 text-teal-400 hover:text-teal-700"
               >
                 Learn more
                 <svg
@@ -43,11 +44,11 @@ const Banner = () => {
                 >
                   <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
                 </svg>
-              </a>
+              </Link>
             </div>
             <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
-              <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
-                <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
+              <div className="bg-black bg-opacity-40 rounded shadow-2xl p-7 sm:p-10">
+                <h3 className="mb-4 text-white text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
                   Sign up for updates
                 </h3>
                 <form>
@@ -102,12 +103,12 @@ const Banner = () => {
                   <div className="mt-4 mb-2 sm:mb-4">
                     <button
                       type="submit"
-                      className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                      className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none border-2 rounded-full"
                     >
                       Subscribe
                     </button>
                   </div>
-                  <p className="text-xs text-gray-600 sm:text-sm">
+                  <p className="text-xs text-white sm:text-sm">
                     We respect your privacy. Unsubscribe at any time.
                   </p>
                 </form>
