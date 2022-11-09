ef93d225808bcdcbfc68e463027e3e2039a70ded
developer-mahin <mdmahin1310@gmail.com>
2022-11-09 16:33:03 +0600

    added blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,11 +1,198 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Blog = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="text-center py-5">
+        <p className="inline-block px-3 py-px text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-400">
+          BLOG
+        </p>
+        <h2 className="text-3xl capitalize font-bold">READ OUR BLOGS</h2>
+      </div>
+      <section className="dark:bg-gray-800 dark:text-gray-100">
+        <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+          <Link
+            rel="noopener noreferrer"
+            className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900"
+          >
+            <img
+              src="https://www.kadamtech.com/wp-content/uploads/2019/01/sql-vs-no-sql-1.png"
+              alt=""
+              className="object-fill w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500"
+            />
+            <div className=" space-y-2 lg:col-span-5">
+              <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">
+                Difference between SQL and NoSQL
+              </h3>
+              <span className="text-xs dark:text-gray-400">
+                November 09, 2022
+              </span>
+              <p>
+                <p>
+                  Remember that SQL dialects share many properties though they
+                  interface with distinct databases. Flavors of NoSQL vary far
+                  more across their attendant systems, so comparison can be more
+                  useful between multiple non-relational technologies vs. SQL
+                  generally.
+                </p>
+
+                <p>
+                  Perhaps the most recognizable SQL dialect is MySQL, an open
+                  source and free RDBMS (though available through proprietary
+                  licenses as well). Its use is widespread in web applications,
+                  and it is known for compatibility, support, and good
+                  performance in the average case. MySQL has also made
+                  concessions to NoSQL practitioners with features like a JSON
+                  data type, the “Document Store,” and support for sharding
+                  (horizontal scaling).
+                </p>
+              </p>
+            </div>
+          </Link>
+          <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            <Link
+              rel="noopener noreferrer"
+              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 border shadow rounded"
+            >
+              <img
+                role="presentation"
+                className="object-cover w-full rounded h-44 dark:bg-gray-500"
+                src="https://miro.medium.com/max/800/1*ulCspc56K_swYE1uuel_TA.png"
+                alt=""
+              />
+              <div className="p-6 space-y-2">
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                  What is JWT?
+                </h3>
+                <span className="text-xs dark:text-gray-400">
+                  November 09, 2022
+                </span>
+                <p>
+                  JSON Web Token (JWT) is an open standard (RFC 7519) that
+                  defines a compact and self-contained way for securely
+                  transmitting information between parties as a JSON object.
+                  This information can be verified and trusted because it is
+                  digitally signed. JWTs can be signed using a secret (with the
+                  HMAC algorithm) or a public/private key pair using RSA or
+                  ECDSA.
+                </p>
+              </div>
+            </Link>
+            <Link
+              rel="noopener noreferrer"
+              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 border shadow rounded"
+            >
+              <img
+                role="presentation"
+                className="object-cover w-full rounded h-44 dark:bg-gray-500"
+                src="https://uploads-ssl.webflow.com/61bb34defcff34f786b458ce/620e820c022217b8ff36eca0_JWT%20Authentication%20in%20React%20Scheme.jpg"
+                alt=""
+              />
+              <div className="p-6 space-y-2">
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                  How does JWT work?
+                </h3>
+                <span className="text-xs dark:text-gray-400">
+                  November 09, 2022
+                </span>
+                <p>
+                  JWTs differ from other web tokens in that they contain a set
+                  of claims. Claims are used to transmit information between two
+                  parties. What these claims are depends on the use case at
+                  hand. For example, a claim may assert who issued the token,
+                  how long it is valid for, or what permissions the client has
+                  been granted.
+                  <br />
+                  Once decoded, you will get two JSON strings:
+                  <ul>
+                    <li className="list-decimal">
+                      The header and the payload.
+                    </li>
+                    <li className="list-decimal">The signature.</li>
+                  </ul>
+                </p>
+              </div>
+            </Link>
+            <Link
+              rel="noopener noreferrer"
+              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 border shadow rounded"
+            >
+              <img
+                role="presentation"
+                className="object-cover w-full rounded h-44 dark:bg-gray-500"
+                src="https://blog.boot.dev/img/800/Nodejs-vs-Javascript-min.webp"
+                alt=""
+              />
+              <div className="p-6 space-y-2">
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                  What What is the difference between javascript and Node JS?
+                </h3>
+                <span className="text-xs dark:text-gray-400">
+                  November 09, 2022
+                </span>
+                <p>
+                  Javascript is a Scripting language. It is mostly abbreviated
+                  as JS. It can be said that Javascript is the updated version
+                  of the ECMA script. Javascript is a high-level programming
+                  language that uses the concept of Oops but it is based on
+                  prototype inheritance.
+                </p>
+                <p>
+                  NodeJS is a cross-platform and opensource Javascript runtime
+                  environment that allows the javascript to be run on the
+                  server-side. Nodejs allows Javascript code to run outside the
+                  browser. Nodejs comes with a lot of modules and mostly used in
+                  web development.{" "}
+                </p>
+              </div>
+            </Link>
+          </div>
+          <Link
+            rel="noopener noreferrer"
+            className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900"
+          >
+            <img
+              src="https://miro.medium.com/max/1200/1*9lmHM-CypCHEOtNd_dfRRw.png"
+              alt=""
+              className="object-fill w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500"
+            />
+            <div className=" space-y-2 lg:col-span-5">
+              <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">
+                How does NodeJS handle multiple requests at the same time?
+              </h3>
+              <span className="text-xs dark:text-gray-400">
+                November 09, 2022
+              </span>
+              <p>
+                <p>
+                  NodeJS receives multiple client requests and places them into
+                  EventQueue. NodeJS is built with the concept of event-driven
+                  architecture. NodeJS has its own EventLoop which is an
+                  infinite loop that receives requests and processes them.
+                </p>
+                <p>
+                  node. js can process upwards of 1000 requests per second and
+                  speed limited only to the speed of your network card. Note
+                  that it's 1000 requests per second not clients connected
+                  simultaneously. It can handle the 10000 simultaneous clients
+                  without issue.
+                </p>
+              </p>
+            </div>
+          </Link>
+          <div className="flex justify-center">
+            <button
+              type="button"
+              className="px-6 py-3 text-sm font-semibold hover:underline bg-cyan-400 rounded-full  "
+            >
+              Load more posts...
+            </button>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Blog;
--- src/pages/Shared/ServiceDetails/ServiceDetails.jsx
+++ src/pages/Shared/ServiceDetails/ServiceDetails.jsx
@@ -1,6 +1,7 @@
 import { Rating } from "flowbite-react";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useReducer, useState } from "react";
 import toast from "react-hot-toast";
+import { FaStarHalfAlt } from "react-icons/fa";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../../context/AuthProvider";
 import PublicReview from "../../ServicesPage/PublicReview/PublicReview";
@@ -9,13 +10,13 @@ const ServiceDetails = () => {
   const { user } = useContext(AuthContext);
   const service = useLoaderData().data;
   const [reviews, setReviews] = useState([]);
+  const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
   const { picture, title, description, _id } = service;
 
   const handlePostReview = (event) => {
     event.preventDefault();
     const form = event.target;
     const textArea = form.review.value;
-    // console.log(textArea);
 
     const reviewInfo = {
       name: user?.displayName,
@@ -35,8 +36,8 @@ const ServiceDetails = () => {
       .then((res) => res.json())
       .then((data) => {
         toast.success("Review Added Successfully");
-        console.log(data.data);
       });
+      forceUpdate()
   };
 
   useEffect(() => {
@@ -44,13 +45,20 @@ const ServiceDetails = () => {
       .then((res) => res.json())
       .then((data) => {
         setReviews(data.data);
-        console.log(data.data);
       });
-  }, [_id]);
+  }, [_id, ignored]);
 
   return (
     <div>
-      <div className="container mx-auto">
+      <div className="container mx-auto py-6">
+        <div className="text-center py-8">
+          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-400">
+            SERVICE DETAILS
+          </p>
+          <h2 className="text-3xl  font-bold uppercase">
+            Read Service details
+          </h2>
+        </div>
         <div className="">
           <img
             src={picture}
@@ -67,20 +75,22 @@ const ServiceDetails = () => {
         {user?.uid ? (
           <>
             {" "}
-            <div className="container mx-auto">
+            <div className="container mx-auto px-3 md:px-0">
               <form onSubmit={handlePostReview}>
                 <textarea
                   name="review"
                   id=""
                   cols="30"
-                  rows="10"
-                  className="w-full"
+                  rows="4"
+                  placeholder="Write your review..."
+                  className="lg:w-1/2 w-full rounded shadow my-3"
                 ></textarea>
+                <br />
                 <button
                   className="bg-cyan-400 hover:bg-cyan-500 rounded-full px-8 py-2 font-semibold"
                   type="submit"
                 >
-                  Post
+                  Review
                 </button>
               </form>
 
@@ -91,25 +101,34 @@ const ServiceDetails = () => {
                     <Rating.Star />
                     <Rating.Star />
                     <Rating.Star />
-                    <Rating.Star filled={false} />
+                    <FaStarHalfAlt className="text-yellow-400"></FaStarHalfAlt>
                     <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
                       4.95 out of 5
                     </p>
                   </Rating>
-                  <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                    1,745 global ratings
-                  </p>
-                  <Rating.Advanced className="my-3" percentFilled={95}>5 star</Rating.Advanced>
-                  <Rating.Advanced className="my-3" percentFilled={3}>4 star</Rating.Advanced>
-                  <Rating.Advanced className="my-3" percentFilled={1}>3 star</Rating.Advanced>
-                  <Rating.Advanced className="my-3" percentFilled={1}>2 star</Rating.Advanced>
-                  <Rating.Advanced className="my-3" percentFilled={0}>1 star</Rating.Advanced>
+                  <Rating.Advanced className="my-3" percentFilled={95}>
+                    5 star
+                  </Rating.Advanced>
+                  <Rating.Advanced className="my-3" percentFilled={3}>
+                    4 star
+                  </Rating.Advanced>
+                  <Rating.Advanced className="my-3" percentFilled={1}>
+                    3 star
+                  </Rating.Advanced>
+                  <Rating.Advanced className="my-3" percentFilled={1}>
+                    2 star
+                  </Rating.Advanced>
+                  <Rating.Advanced className="my-3" percentFilled={0}>
+                    1 star
+                  </Rating.Advanced>
                 </React.Fragment>
+
                 <div className="grid lg:grid-cols-2">
-                  {reviews.map((review) => (
+                  {reviews.map((reviewInfo) => (
+                    // console.log(reviewInfo)
                     <PublicReview
-                      key={review._id}
-                      review={review}
+                      key={reviewInfo._id}
+                      reviewInfo={reviewInfo}
                     ></PublicReview>
                   ))}
                 </div>
@@ -119,11 +138,11 @@ const ServiceDetails = () => {
         ) : (
           <>
             <h2 className="text-3xl font-bold text-center capitalize text-cyan-500">
-              if you want to see the review then{" "}
+              Please{" "}
               <Link className="underline text-blue-600" to="/login">
                 login
               </Link>{" "}
-              first
+              To add a review
             </h2>
           </>
         )}
