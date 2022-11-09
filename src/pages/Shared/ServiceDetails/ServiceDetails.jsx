f0bf707e24ebc5cbc90e1c373afe537620218bcf
developer-mahin <mdmahin1310@gmail.com>
2022-11-09 14:11:15 +0600

    added reting details page

--- src/pages/Shared/ServiceDetails/ServiceDetails.jsx
+++ src/pages/Shared/ServiceDetails/ServiceDetails.jsx
@@ -1,9 +1,15 @@
-import React, { useContext } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Rating } from "flowbite-react";
+import React, { useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../../context/AuthProvider";
+import PublicReview from "../../ServicesPage/PublicReview/PublicReview";
 
 const ServiceDetails = () => {
   const { user } = useContext(AuthContext);
+  const service = useLoaderData().data;
+  const [reviews, setReviews] = useState([]);
+  const { picture, title, description, _id } = service;
 
   const handlePostReview = (event) => {
     event.preventDefault();
@@ -27,11 +33,21 @@ const ServiceDetails = () => {
       body: JSON.stringify(reviewInfo),
     })
       .then((res) => res.json())
-      .then((data) => console.log(data.data));
+      .then((data) => {
+        toast.success("Review Added Successfully");
+        console.log(data.data);
+      });
   };
 
-  const service = useLoaderData().data;
-  const { picture, title, description, _id } = service;
+  useEffect(() => {
+    fetch(`http://localhost:5000/review/${_id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data.data);
+        console.log(data.data);
+      });
+  }, [_id]);
+
   return (
     <div>
       <div className="container mx-auto">
@@ -47,22 +63,70 @@ const ServiceDetails = () => {
           <p>{description}</p>
         </div>
       </div>
-      <div className="container mx-auto">
-        <form action="" onSubmit={handlePostReview}>
-          <textarea
-            name="review"
-            id=""
-            cols="30"
-            rows="10"
-            className="w-full"
-          ></textarea>
-          <button
-            className="bg-cyan-400 hover:bg-cyan-500 rounded-full px-8 py-2 font-semibold"
-            type="submit"
-          >
-            Post
-          </button>
-        </form>
+      <div className="py-5">
+        {user?.uid ? (
+          <>
+            {" "}
+            <div className="container mx-auto">
+              <form onSubmit={handlePostReview}>
+                <textarea
+                  name="review"
+                  id=""
+                  cols="30"
+                  rows="10"
+                  className="w-full"
+                ></textarea>
+                <button
+                  className="bg-cyan-400 hover:bg-cyan-500 rounded-full px-8 py-2 font-semibold"
+                  type="submit"
+                >
+                  Post
+                </button>
+              </form>
+
+              <div className="py-4">
+                <React.Fragment>
+                  <Rating>
+                    <Rating.Star />
+                    <Rating.Star />
+                    <Rating.Star />
+                    <Rating.Star />
+                    <Rating.Star filled={false} />
+                    <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
+                      4.95 out of 5
+                    </p>
+                  </Rating>
+                  <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                    1,745 global ratings
+                  </p>
+                  <Rating.Advanced className="my-3" percentFilled={95}>5 star</Rating.Advanced>
+                  <Rating.Advanced className="my-3" percentFilled={3}>4 star</Rating.Advanced>
+                  <Rating.Advanced className="my-3" percentFilled={1}>3 star</Rating.Advanced>
+                  <Rating.Advanced className="my-3" percentFilled={1}>2 star</Rating.Advanced>
+                  <Rating.Advanced className="my-3" percentFilled={0}>1 star</Rating.Advanced>
+                </React.Fragment>
+                <div className="grid lg:grid-cols-2">
+                  {reviews.map((review) => (
+                    <PublicReview
+                      key={review._id}
+                      review={review}
+                    ></PublicReview>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <h2 className="text-3xl font-bold text-center capitalize text-cyan-500">
+              if you want to see the review then{" "}
+              <Link className="underline text-blue-600" to="/login">
+                login
+              </Link>{" "}
+              first
+            </h2>
+          </>
+        )}
       </div>
     </div>
   );
